577d71eb6940d711062e49ff6ab90fe58358dfeb
Dima Smaha <fannifty@gmail.com>
2023-11-30 20:19:59 +0200

    typu

--- reqres.js
+++ reqres.js
@@ -10,11 +10,11 @@ export const options = {
 
 function check_response(res) {
   check(res, {
-    "is status 200": (r) => r.status === 200,
-    "response body not empty": (r) => r.body.length > 0,
-    "response time is less than 200ms": (r) => r.timings.duration < 200,
-    "is JSON": (r) => r.headers["Content-Type"] === "application/json",
-    "contains expected ID": (r) => JSON.parse(r.body).data[0].id === 7,
+    "is status 200": (r) => r.status === 200, //status code
+    "response body not empty": (r) => r.body.length > 0, // body have some value
+    "response time is less than 200ms": (r) => r.timings.duration < 200, //response time less 200ms
+    "is JSON": (r) => r.headers["Content-Type"] === "application/json", // resposponse is JSON
+    "contains expected ID": (r) => JSON.parse(r.body).data[0].id === 7, //inside of data we have ID
   });
 }
 
